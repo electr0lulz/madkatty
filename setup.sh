80a40965c53bdac321d1cf4e54413fd6c730b90f
electr0lulz <electrolulz@protonmail.com>
2022-07-30 14:15:44 +0200

    update

--- setup.sh
+++ setup.sh
@@ -4,18 +4,12 @@ DEFPROXY="./ddos/config.json"
 
 apt-get update -y
 apt-get upgrade -y
-
-
+apt-get install -y net-tools python3 python3-pip golang-go gcc make curl wget ntp 
 ## DDoS Builder ##
 cd $DDOS
-apt-get install -y python3
-apt-get install -y pip3
-apt-get install -y pip
 pip install -r requirements.txt
 pip3 install -r requirements.txt
-
 ## Bombardier ##
-sudo apt-get install -y golang-go
 cd ddos
 mkdir bombardier_tmp
 cd bombardier_tmp
@@ -24,7 +18,10 @@ go mod edit -replace github.com/codesenberg/bombardier=github.com/PXEiYyMH8F/bom
 go get github.com/codesenberg/bombardier
 cd ..
 rm -r bombardier_tmp
-cd
+cd files
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt-get install ./resources/google-chrome-stable_current_amd64.deb
+# anonsurf #
 git clone https://github.com/Und3rf10w/kali-anonsurf.git
 cd kali-anonsurf
 chmod +x installer.sh
